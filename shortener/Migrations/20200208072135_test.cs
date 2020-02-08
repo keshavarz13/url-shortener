5bef7704bed9a46b8a29af06acfa9d4519ef0e55
keshavarz13 <keshavarz.ma13@gmail.com>
2020-02-08 15:36:18 +0330

    Add database migration

---
+++ shortener/Migrations/20200208072135_test.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace shortener.Migrations
+{
+    public partial class test : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "url",
+                columns: table => new
+                {
+                    ShortUrl = table.Column<string>(nullable: false),
+                    LongUrl = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_url", x => x.ShortUrl);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_url_ShortUrl",
+                table: "url",
+                column: "ShortUrl",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "url");
+        }
+    }
+}
